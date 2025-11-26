18c5e21b99340d22c65b80bc113e5f9b0ca85c0d
Vincent Canaguy <vincent.canaguy@grenoble-inp.org>
2025-11-26 15:23:03 +0100

    fix merge issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,3 @@
-
-// world map & scatterplot selection
-
-if (document.querySelector("#world-map-chart")) {
-  createWorldMap();
-  createTimelineScale();
-}
-
-if (document.querySelector("#scatterplot-chart")) {
-  createScatterPlot(rawData, CHART_CONFIG);
-}
-
 // NAVBAR code
 document.addEventListener('DOMContentLoaded', () => {
   const currentPage = window.location.pathname.split('/').pop() || 'index.html';
